b21d2e45477b1bfc3a91564d32cc97a2e545ce1d
xamfp <xamfp@protonmail.com>
2017-06-24 23:06:36 -0500

    fix tab

--- generate.py
+++ generate.py
@@ -6,7 +6,7 @@ vuln_url = 'http://127.0.0.1:5000/vulnerable.html'              # Vulnerable Pag
 spoof_page_domain = 'whitehouse.gov'                            # Domain Name of Spoof Page example.com
 spoof_page_full_url = 'http://whitehouse.gov'                   # Full Screen iframe URL
 spoof_page_title = 'The White House'                            # Page Title Name
-custom_js = 'alert("I control the content here")'     # Custom Javascript
+custom_js = 'alert("I control the content here")'               # Custom Javascript
 ## End of Config
 
 app = Flask(__name__, static_url_path='', static_folder='web/static', template_folder='web/templates')
